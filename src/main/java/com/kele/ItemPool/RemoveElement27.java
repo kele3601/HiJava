fa89e9f865a0c5ab9b68129d8a0c38143c5c6642
可乐炖鸡翅 <907598259@qq.com>
2022-10-12 00:11:13 +0800

    Day01.二分+双指针

---
+++ src/main/java/com/kele/ItemPool/RemoveElement27.java
@@ -0,0 +1,25 @@
+package com.kele.ItemPool;
+
+/**
+ * @author: kele3601
+ * @date: 2022/10/11 23:27
+ * @description: 27. 移除元素
+ */
+public class RemoveElement27 {
+
+    /**
+    * @Description:
+    * @Date: 2022/10/11
+    * @Author: kele3601
+    */
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
---
+++ src/main/java/com/kele/ItemPool/Search704.java
@@ -0,0 +1,45 @@
+package com.kele.ItemPool;
+
+/**
+ * @author: kele3601
+ * @date: 2022/10/11 21:45
+ * @description: 704. 二分查找
+ */
+public class Search704 {
+
+
+    /**
+     * 二分查找步骤：
+     *      1. 找值：找到中位数
+     *      2. 比较：比较大小
+     *      3. 处理：if（num>target）{}else{}
+     */
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            int num = nums[mid];
+
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+//    public int search(int[] nums, int target) {
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == target) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+}
