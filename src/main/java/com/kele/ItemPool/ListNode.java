5488e73b7be1a64118c523038c712779102c4ba0
可乐炖鸡翅 <907598259@qq.com>
2022-10-14 12:08:52 +0800

    增：链表

---
+++ src/main/java/com/kele/ItemPool/ListNode.java
@@ -0,0 +1,14 @@
+package com.kele.ItemPool;
+
+/**
+ * @author: kele3601
+ * @date: 2022/10/14 10:07
+ * @description:
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(){}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
